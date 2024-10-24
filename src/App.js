7c8063b26434503a2a227247834cf1c443d4bd83
Jinyoung Kim <gojillajin@gmail.com>
2024-10-24 13:08:01 +0900

    Create customer.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
 import './App.css';
 import Customer from './components/Customer.js';
 
-const customer = {
+const customers = [
+  {
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/1',
   'name' : '홍길동',
   'birthday' : '850117',
   'gender' : '남자',
   'job' : '대학생'
+},
+{
+  'id' : 2,
+  'image' : 'https://placeimg.com/64/64/2',
+  'name' : '김진영',
+  'birthday' : '950117',
+  'gender' : '남자',
+  'job' : '교사'
+},
+{
+  'id' : 3,
+  'image' : 'https://placeimg.com/64/64/3',
+  'name' : '조예림',
+  'birthday' : '951023',
+  'gender' : '여자',
+  'job' : '디자이너'
 }
+]
 
 function App() {
   return (
-    <Customer
-      name={customer.name}
-      birthday={customer.birthday}
-      gender={customer.gender}
-      job={customer.job}
-    />
+    <div>
+      { customers.map(a => { return ( <Customer key={a.id} id={a.id} img={a.image} name={a.name} birthday={a.birthday} gender={a.gender} job={a.job} />)})}
+    </div>
   );
 }
 
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,13 +1,30 @@
-function Customer({name, birthday, gender, job}) {
+function Customer({id, img, name, birthday, gender, job}) {
     return(
         <>
         <div>
-            <h2>{name}</h2>
-            <p>8{birthday}</p>
+            <CustomerProfile name={name} id={id} img={img} />
+            <CustomerInfo  birthday={birthday} gender={gender} job={job} />
+        </div>
+        </>
+    )
+}
+
+function CustomerProfile({name, id, img}) {
+    return(
+        <div>
+            <img src={img} alt="profile" />
+            <h2>{name}({id})</h2>
+        </div>
+    )
+}
+
+function CustomerInfo({birthday, gender, job}) {
+    return (
+        <div>
+            <p>{birthday}</p>
             <p>{gender}</p>
             <p>{job}</p>
         </div>
-        </>
     )
 }
 
